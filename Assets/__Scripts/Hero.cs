6928eecece64e00968201012e821d9031327923f
Yusra Irfan <yusrairfan99@hotmail.com>
2019-02-25 16:26:50 -0500

    Revert "renamed"
    This reverts commit b4707cd583d406594268dbb43bc7fbbd40414c5f.

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -23,7 +23,12 @@ public class Hero : MonoBehaviour
 		}
 	}
 
-  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
     // Update is called once per frame
     void Update()
     {
